7dcd4a15c9b8850d7062e6f644c3fd896a2dba37
David L. Goldberg <davidlgoldberg@gmail.com>
2012-12-20 04:08:16 -0500

    Switched over to use ordered hash object.

--- github_parse/parseUserPage.js
+++ github_parse/parseUserPage.js
@@ -3,7 +3,7 @@ var links = $('.commits code a')
         return $(this).attr('href');
     }).get();
 
-var changeSets = [];
+var changeSets = {};
 
 function getNumber (changeSetPart) {
     var numberPattern = /[0-9]+/g;
@@ -14,12 +14,10 @@ $.each(links, function (index, value) {
     $('<p>').load(value + ' ' + '#toc .explain', function() {
         var $changeSet = $(this).find('strong');
 
-        changeSets.push(
-            {
-                'changed': getNumber($changeSet[0]),
-                'additions': getNumber($changeSet[1]),
-                'deletions': getNumber($changeSet[2])
-            }
-        );
+        changeSets[index] = {
+            'changed': getNumber($changeSet[0]),
+            'additions': getNumber($changeSet[1]),
+            'deletions': getNumber($changeSet[2])
+        };
     });
 });
